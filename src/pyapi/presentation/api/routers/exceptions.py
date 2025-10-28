5664cee4da41489e120b29050177797a5321eaa3
SamWarden <SamWardenSad@gmail.com>
2025-10-28 22:46:00 +0300

    feat(exception): add `ExceptionHandler`

--- src/pyapi/presentation/api/routers/exceptions.py
+++ src/pyapi/presentation/api/routers/exceptions.py
@@ -1,5 +1,6 @@
 import logging
-from collections.abc import Awaitable, Callable
+from collections.abc import Callable
+from dataclasses import dataclass
 
 from fastapi import Request, status
 from starlette.applications import Starlette
@@ -13,38 +14,51 @@ logger = logging.getLogger(__name__)
 
 
 def setup_exceptions_handlers(app: Starlette) -> None:
-    app.add_exception_handler(
-        InvalidIdentityError,
-        error_handler(
-            http_code=status.HTTP_401_UNAUTHORIZED,
-            code=ErrorCode.INVALID_IDENTITY_ERROR,
-        ),
+    add_exception_handlers(
+        app,
+        [
+            ExceptionHandler(
+                exception=InvalidIdentityError,
+                http_code=status.HTTP_401_UNAUTHORIZED,
+                code=ErrorCode.INVALID_IDENTITY_ERROR,
+            ),
+            ExceptionHandler(
+                exception=Exception,
+                http_code=status.HTTP_500_INTERNAL_SERVER_ERROR,
+                code=ErrorCode.INTERNAL_ERROR,
+                get_data=lambda err: {},
+                log_level="ERROR",
+            ),
+        ],
     )
-    app.add_exception_handler(
-        Exception,
-        error_handler(
-            http_code=status.HTTP_500_INTERNAL_SERVER_ERROR,
-            code=ErrorCode.INTERNAL_ERROR,
-            get_data=lambda err: {},
-        ),
-    )
 
 
-def error_handler[E: Exception](
-    http_code: int,
-    code: ErrorCode,
-    get_data: Callable[[E], dict[str, object]] | None = None,
-) -> Callable[[Request, E], Awaitable[ORJSONResponse]]:
-    if get_data is None:
-        get_data = _default_get_data
+def add_exception_handlers(app: Starlette, handlers: list["ExceptionHandler[Exception]"]) -> None:
+    for handler in handlers:
+        app.add_exception_handler(handler.exception, handler)
 
-    async def _handle(request: Request, err: E) -> ORJSONResponse:
-        error_data = ErrorResponse(id=request.state.request_id, code=code, data=get_data(err))
+
+def _default_get_data(err: object) -> dict[str, object]:
+    return dict(vars(err))
+
+
+@dataclass(slots=True, frozen=True)
+class ExceptionHandler[E]:
+    exception: type[E]
+    http_code: int
+    code: ErrorCode
+    get_data: Callable[[E], dict[str, object]] = _default_get_data
+    log_level: str = "WARNING"
+
+    async def __call__(self, request: Request, err: E) -> ORJSONResponse:
+        error_data = ErrorResponse(id=request.state.request_id, code=self.code, data=self.get_data(err))
         error_response = ORJSONResponse(
             content={"error": error_data},
-            status_code=http_code,
+            status_code=self.http_code,
         )
-        logger.error(
+        exception_info: dict[str, E] = {"exc_info": err} if self.log_level == "ERROR" else {}
+        logger.log(
+            logging.getLevelName(self.log_level),
             "An error occurred: %r",
             err,
             extra={
@@ -55,11 +69,6 @@ def error_handler[E: Exception](
                 },
                 "status_code": error_response.status_code,
             },
+            **exception_info,  # type: ignore[arg-type]
         )
         return error_response
-
-    return _handle
-
-
-def _default_get_data(err: Exception) -> dict[str, object]:
-    return dict(vars(err))
